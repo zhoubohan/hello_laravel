d965f5d79ebb455be43f7518065ae254a546f2bf
zhoubohan <zhoubohan.pro@gmail.com>
2018-03-27 15:49:59 +0800

    finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="">
+            <a href="">
+
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+
+</div>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello IRENE</h1>
+        <p class="lead">
+            您现在看到的是IRENE APP
+        </p>
+        <P>
+            一切从这里开始
+        </P>
+        <p>
+            <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
+        </p>
+    </div>
+@stop
