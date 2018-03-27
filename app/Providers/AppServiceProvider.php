1700e63eb703e545acb38333ea700c85cd79b45a
zhoubohan <zhoubohan.pro@gmail.com>
2018-03-27 16:08:40 +0800

    move users mode to models folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //解决migrate字段过长问题
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -25,4 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+
+
 }
